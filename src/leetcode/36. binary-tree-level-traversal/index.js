9053121591b1f10cdb3eed459adc8efc0a5a3de0
Van Thong <thonglevan131@gmail.com>
2020-07-22 17:01:35 +0700

    updated solutions for  'binary-tree-postorder' and 'binary-tree-level-traversal'

--- src/leetcode/36. binary-tree-level-traversal/index.js
+++ src/leetcode/36. binary-tree-level-traversal/index.js
@@ -10,20 +10,20 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
+var levelOrder = function (root) {
   if (!root) return [];
   let output = [];
   let queue = [root];
-  while(queue.length) {
-      let size = queue.length;
-      let currentLevel = [];
-      for (let i = 0; i < size; i++) {
-          let current = queue.shift();
-          currentLevel.push(current.val);
-          if (current.left) {queue.push(current.left)}
-          if (current.right) {queue.push(current.right)}
-      }
-      output.push(currentLevel)
+  while (queue.length) {
+    let size = queue.length;
+    let currentLevel = [];
+    for (let i = 0; i < size; i++) {
+      let current = queue.shift();
+      currentLevel.push(current.val);
+      if (current.left) { queue.push(current.left) }
+      if (current.right) { queue.push(current.right) }
+    }
+    output.push(currentLevel)
   }
   return output;
 };
