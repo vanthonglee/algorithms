c103243cf9398f0504cb176b63291b2809d9b50b
Van Thong <thonglevan131@gmail.com>
2020-07-22 16:38:38 +0700

    added binary-tree-postorder

---
+++ src/leetcode/35. binary-tree-postorder/recursive.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root, output = []) {
+  if (!root) {
+    output = []
+  } else {
+    postorderTraversal(root.left, output);
+    postorderTraversal(root.right, output);
+    output.push(root.val);
+  }
+  return output;
+};
